c3a98c5b15523e4d426cbae691bdf3262d495017
Rick Belson <belsonrj@aol.com>
2020-02-20 02:14:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,2 @@
-require "pry"
 class Dog
-  attr_reader :mood
-  attr_accessor :name, :owner
   
-  @mood = "nervous"
-
-  def initialize
-    @name = name
-    @mood = mood
-    @dogs = []
-  end
-
-  def self.all 
-    @dogs
-  end
-end
