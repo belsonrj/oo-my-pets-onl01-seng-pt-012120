e37db6bbfeb05ab599e4ceed30b799ffdefd64d7
Rick Belson <belsonrj@aol.com>
2020-02-20 02:12:10 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -54,8 +54,9 @@ class Cat
     end
   end
   
-    def list_pets
-      number_of_dogs = self.dogs.count
-      number_of_cats = self.cats.count
-      
+  def list_pets
+    number_of_dogs = self.dogs.count
+    number_of_cats = self.cats.count
+    "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
+  end
 end
