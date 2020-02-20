2b00b8b29f31c0b3f5ba3507406ff94ef4ed1579
Rick Belson <belsonrj@aol.com>
2020-02-20 02:22:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,4 +59,9 @@ class Owner
     number_of_cats = self.cats.count
     "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
   end
+
+  def self.reset_all
+    self.all.clear
+  end
+end
 end
