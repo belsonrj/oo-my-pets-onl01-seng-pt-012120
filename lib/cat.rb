1bb9641dbb9b134d13da81fdf2c9051f663e380b
Rick Belson <belsonrj@aol.com>
2020-02-20 02:00:35 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
-  # code goes here
+  attr_reader :mood
+  attr_accessor :name, :owner
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+  end
+
+  def self.all 
+    @@all 
+  end
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
+require "pry"
 class Dog
-  # code goes here
+  attr_reader :mood
+  attr_accessor :name, :owner
+  
+  @mood = "nervous"
+
+  def initialize
+    @name = name
+    @mood = mood
+    @dogs = []
+  end
+
+  def self.all 
+    @dogs
+  end
 end
