1c904adb9b8a9d0d9e22bdff545ada700293c5d4
Rick Belson <belsonrj@aol.com>
2020-02-20 02:03:49 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,17 +1,54 @@
 class Cat
-  attr_reader :mood
-  attr_accessor :name, :owner
+  attr_accessor :cats, :dogs
+  attr_reader :species, :name
 
   @@all = []
 
+
   def initialize(name)
     @name = name
-    @owner = owner
-    @mood = "nervous"
+    @species = "human"
+    @@all << self
+    @cats = []
+    @dogs = []
+  end  
+  
+    def self.all 	  end
+      @@all 	
+    end	  def self.all
+
+    @@all
+    def self.count	  end
+        self.all.length	
+    end	  def self.count
+
+    self.all.length
+    def say_species	  end
+        "I am a #{self.species}."	
+    end	  def say_species
+
+    "I am a #{self.species}."
+    def buy_cat(name)	  end
+        Cat.new(name, self)	
+    end	  def buy_cat(name)
+
+    Cat.new(name, self)
+    def buy_dog(name)	  end
+        Dog.new(name, self)	
+  def buy_dog(name)
+    Dog.new(name, self)
   end
 
-  def self.all 
-    @@all 
+  def walk_dogs
+   self.dogs.each {|dog| dog.mood = "happy"}
   end
 
-end
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.cats.each do |cat|
+      self.cats.delete(cat)
+      cat.owner = nil
+      cat.mood = "nervous"
